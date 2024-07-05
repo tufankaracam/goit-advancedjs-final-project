07ab703fb4073f79566a113be28cd71e5d63f42b
Alex Kydanov <a.kidanov@gmail.com>
2024-07-05 20:26:07 +0300

    lazyloading (#34)

--- src/js/category-card-markup.js
+++ src/js/category-card-markup.js
@@ -1,12 +1,12 @@
 /**create markup for category card
- * 
- * @param {object} data 
+ *
+ * @param {object} data
  * @returns {string}
  */
 export const createCategoryMarkup = data =>
   data.map(({ filter = 'Not found', name = 'Not found', imgURL }) =>
       `<div class="category-wrap" name="${name}">
-      <img class="category-img" src="${imgURL}" alt="${name}">
+      <img class="category-img" src="${imgURL}" alt="${name}" loading="lazy">
       <div class="category-text-wrap">
           <p class="category-title">${name}</p>
           <p class="category-subtitle">${filter}</p>
